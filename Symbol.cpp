390150ad5269c1c133636e4ccdd20f96531216f1
EgbariaMohammad <agbariya@campus.technion.ac.il>
2024-08-22 02:17:35 +0300

    change Symbol Interface

--- Symbol.cpp
+++ Symbol.cpp
@@ -6,14 +6,29 @@
 using namespace std;
 
 
-Symbol::Symbol(const string name, const string type, string variable , int position)
-    : name(name), type(type), variable(variable), offset(position)  {}
+Symbol::Symbol(const string& type, const string& name, string registerName, int offset)
+    : type(type), name(name), regName(registerName), offset(offset)  {}
 
+
+const string& Symbol::getType() const { return type; }
 const string& Symbol::getName() const { return name; }
-string Symbol::getType() const { return type; }
+const string& Symbol::getRegName() const { return regName; }
 int Symbol::getOffset() const { return offset; }
 
-Function::Function(const string name, const string type, const string variable, int pos, string returnType, vector<string> parametersTypes) : Symbol(name, type, variable, pos), returnType(returnType), parametersTypes(parametersTypes) {}
+void  Symbol::setType(const string& type) { this->type = type; }
+void  Symbol::setName(const string& name) { this->name = name; }
+void  Symbol::setRegName(const string& registerName) { this->regName = registerName; }
+void  Symbol::setOffset(int offset) { this->offset = offset; }
+
+
+Num::Num(const string& name, int value, string registerName, int offset) : Symbol("INT", name, registerName, offset), val(value) {}
+int Num::getValue() const { return val; }
+
+myString::myString(const string& name, const string& value, string registerName, int offset) : Symbol("STRING", name, registerName, offset), val(value) {}
+string myString::getStringValue() const { return val; }
+
+Function::Function(const string& name, int offset, string returnType = "", vector<string> parametersTypes = vector<string>()) :
+        Symbol("FUNCTION", name, "NONE", offset), returnType(returnType), parametersTypes(parametersTypes) {}
 
 string Function::getReturnType() const { return returnType; }
 vector<string>& Function::getParametersTypes()  { return parametersTypes; }
--- Symbol.hpp
+++ Symbol.hpp
@@ -18,32 +18,32 @@ using namespace std;
 class Symbol
 {
 private:
+    string type; // INT, STRING, FUNCTION..
     string name;
-    string type;
-    string variable;
+    string regName;
     int offset;
 public:
-    Symbol(const string name, const string type, string variable="NO VARIABLE ALLOCATED", int position = -1);
-
-    Symbol(const Symbol& e);
+    Symbol(const string& type, const string& name, string registerName="NONE", int offset = -1);
+    Symbol(const Symbol& e) = default;
     virtual ~Symbol() = default;
 
+    const string& getType() const;
     const string& getName() const;
-    virtual int getValue() const { return -1; }
-    string getType() const;
+    const string& getRegName() const;
     int getOffset() const;
 
-    string getVariable() const { return variable; }
-    void setVariable(const string& variable) { this->variable = variable; }
-    void setName(const string& name) { this->name = name; }
+    void setType(const string& type);
+    void setName(const string& name);
+    void setRegName(const string& registerName);
+    void setOffset(int offset);
 };
 
 class Num : public Symbol
 {
     int val;
 public:
-    Num(const string name, const string type, const string variable, int position, int val) : Symbol(name, type, variable, position), val(val) {}
-    virtual int getValue() const override { return val; }
+    Num(const string& name, int value, string registerName="NONE", int offset = -1);
+    virtual int getValue() const;
 };
 
 
@@ -51,17 +51,17 @@ class myString : public Symbol
 {
     string val;
 public:
-    myString(const string name, const string type, const string variable, int position, const string val) : Symbol(name, type, variable, position), val(val) {}
-    string getStringValue() const { return val; }
+    myString(const string& name, const string& value, string registerName="NONE", int offset = -1);
+    string getStringValue() const;
 };
 
 class Function : public Symbol {
 private:
     string returnType;
     vector<string> parametersTypes;
-
 public:
-    Function(const string name, const string type, const string variable, int pos, string returnType = "", vector<string> parametersTypes = vector<string>());
+    Function(const string& name, int offset, string returnType = "", vector<string> parametersTypes = vector<string>()) :
+        Symbol("FUNCTION", name, "NONE", offset), returnType(returnType), parametersTypes(parametersTypes) {}
     string getReturnType() const;
     vector<string>& getParametersTypes();
 };
